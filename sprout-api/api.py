5860913f150574796ad39e3ef0a577d726f093a2
extr3mis <37597293+extr3mis@users.noreply.github.com>
2022-05-13 04:52:11 +0530

    Add files via upload

--- sprout-api/api.py
+++ sprout-api/api.py
@@ -13,8 +13,12 @@ from flask import Flask, Response, jsonify, request
 from models import Pump, pHSensor, TDSSensor, WaterLevelSensor
 from helpers import *
 
+# Configuration file
 with open('config.json', 'r') as f:
     config = json.load(f)
+
+with open('../ports.json', 'r') as f:
+    ports = json.load(f)
 
 # Create the SPI bus
 spi = busio.SPI(clock = board.SCK, MISO = board.MISO, MOSI = board.MOSI)
@@ -31,13 +35,15 @@ _tds = TDSSensor(mcp, config['channels']['TDS'])
 _waterlevel = WaterLevelSensor(mcp, config['channels']['WATERLEVEL'], config['pins']['WATERLEVEL_POWER'])
 dht22 = adafruit_dht.DHT22(getPin(config['pins']['DHT22']), use_pulseio = False)
 
+print(config['pins']['M1'], type(config['pins']['M1']))
+
 # Creating Pump objects
 pumps = {   
-            1 : Pump(getPin(config['pins']['M1'])),
-            2 : Pump(getPin(config['pins']['M2'])), 
-            3 : Pump(getPin(config['pins']['M3'])), 
-            4 : Pump(getPin(config['pins']['M4'])), 
-            5 : Pump(getPin(config['pins']['M5']))
+            1 : Pump(config['pins']['M1']),
+            2 : Pump(config['pins']['M2']), 
+            3 : Pump(config['pins']['M3']), 
+            4 : Pump(config['pins']['M4']), 
+            5 : Pump(config['pins']['M5'])
         }
 
 app = Flask(__name__)
@@ -45,6 +51,8 @@ app = Flask(__name__)
 # API ENDPOINTS 
 @app.route('/temp', methods = ['GET'])
 def temperature():
+
+    # While loop is here because communication with DHT tends to fail sometimes.
     while True:
         try: temperature = dht22.temperature
         except RuntimeError: continue
@@ -54,6 +62,8 @@ def temperature():
 
 @app.route('/humidity', methods = ['GET'])
 def humidity():
+    
+    # While loop is here because communication with DHT tends to fail sometimes.
     while True:
         try: humidity = dht22.humidity
         except RuntimeError: continue
@@ -78,20 +88,25 @@ def waterlevel():
 
 @app.route('/pump', methods = ['POST'])
 def pump():
+
+    # Extracting parameters from request.
     number = int(request.args['number'])
     action = str(request.args['action']).lower().strip()
 
+    # Tried to access a pump that does not exist.
     if number not in pumps.keys(): 
-        return Response(status = 400)
+        return Response(status = 404)
     
     pump = pumps[int(request.args['number'])]
 
+    # Invalid action
     if action not in ['activate', 'deactivate', 'toggle']:
-        return Response(status = 400)
+        return Response(status = 405)
     
     _action = getattr(pump, action)
     _action()
     return Response(status = 200)
 
+# This hack uses the command line to get the IPv4 address of the server and then host there
 ip = subprocess.getoutput('hostname -I').strip()
-app.run(host=ip)
+app.run(host = ip, port = ports['sprout-api'])
